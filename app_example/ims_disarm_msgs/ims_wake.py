d57e7c5a4b616cae7a93393c8e7ef1be38a2fc77
Cao Jun <sxha951@gmail.com>
2025-03-30 12:17:51 +0800

    [0330]update
    Description of Change:
    
    Author:
    	sxha951@gmail.com
    Reviewer:
    	sxha951@gmail.com
    Test:
    	Pass

--- app_example/ims_disarm_msgs/ims_wake.py
+++ app_example/ims_disarm_msgs/ims_wake.py
@@ -20,7 +20,7 @@ can.rc['sam_abr'] = 1
 
 
 class CANFrameController:
-    def __init__(self, bus, can_id, initial_data, phase_change_count, intervals):
+    def __init__(self, bus, can_id, initial_data, phase_change_count, intervals, switch_data=1):
         self.bus = bus
         self.can_id = can_id
         self.initial_data = initial_data
@@ -30,10 +30,14 @@ class CANFrameController:
         self.frame_count = 0
         self.running = False
         self.thread = None
+        self.switch_data = switch_data  # New parameter to control data switching
 
     def _send_loop(self):
         while self.running:
-            phase_index = 0 if self.frame_count < self.phase_change_count else 1
+            if self.switch_data:  # Check if data switching is enabled
+                phase_index = 0 if self.frame_count < self.phase_change_count else 1
+            else:
+                phase_index = 0  # If data switching is disabled, use initial data
             msg = can.Message(
                 arbitration_id=self.can_id,
                 data=self.phase_data[phase_index],
@@ -81,6 +85,17 @@ def shell_control():
         intervals=[20, 20]
     )
     
+    controller_2EA = CANFrameController(
+        bus=bus,
+        can_id=0x2EA,
+        initial_data={
+            0: [0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            1: [0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        },
+        phase_change_count=10,
+        intervals=[20, 20]
+    )
+    
     controller_600_arm = CANFrameController(
         bus=bus,
         can_id=0x600,
@@ -109,15 +124,19 @@ def shell_control():
             print("Started CAN NM transmission")
             controller_600.start()
             controller_391.start()
+            controller_2EA.start()
         elif cmd == '2':
             controller_600.stop()
+            controller_2EA.stop()
             controller_391.stop()
             print("Send ARM Msgs to enter polling mode")
+            controller_2EA.start()
             controller_600_arm.start()
             controller_391_arm.start()
             def stop_arm_controllers():
                 controller_600_arm.stop()
                 controller_391_arm.stop()
+                controller_2EA.stop()
                 # bus.shutdown()
                 print("Automatically stopped ARM messages after 5 seconds")
             threading.Timer(5.0, stop_arm_controllers).start()
@@ -125,6 +144,7 @@ def shell_control():
         elif cmd == '3':
             controller_600.stop()
             controller_391.stop()
+            controller_391_arm.stop()
             controller_600_arm.stop()
             controller_391_arm.stop()
             bus.shutdown()
@@ -132,4 +152,3 @@ def shell_control():
 
 if __name__ == "__main__":
     shell_control()
-
